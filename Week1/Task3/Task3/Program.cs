2c97992a8fd50756cb94f873617b1246abfd4add
DamiNo007 <dmoldabayev.99@gmail.com>
2019-01-31 02:04:56 +0600

    test

--- Week1/Task3/Task3/Program.cs
+++ Week1/Task3/Task3/Program.cs
@@ -8,6 +8,23 @@ namespace Task3
 {
     class Program
     {
+        // Function which will make out from an array of integers another array of integers where each element is repeated 2 times
+        public static void NewArr(int[] a, int n) 
+        {
+            List<int> list = new List<int>(); // Создаем динамический массив в который будем закидывать дублированные элементы массива a
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                list.Add(a[i]); // Добавляем в новый массив элементы массива а по два раза
+                list.Add(a[i]);
+            }
+
+            foreach(int k in list)
+            {
+                Console.Write(k+" "); //Выводим элементы динамического массива
+            }
+        }
+
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
@@ -20,12 +37,10 @@ namespace Task3
                 a[i] = int.Parse(arr[i]); // Присваиваем элементам массива введенные значения строки
             }
 
-            foreach (int k in a)
-            {
-                Console.Write(k + " " + k + " "); // Выводим дублируя каждый элемент массива
+            NewArr(a, n); //Вызываем функцию, которая выведет элементы динамического массива
 
-            }
             Console.ReadKey();
         }
     }
-}
+ } 
+
