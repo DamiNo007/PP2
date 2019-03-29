5f429668c2ef6561432d04773af8357e5e5b90a8
DamiNo007 <dmoldabayev.99@gmail.com>
2019-03-30 00:21:23 +0600

    test

---
+++ Week7/Task1/Task1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Task1
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            /*Thread t = Thread.CurrentThread;
+
+            Console.WriteLine(t.Name);
+            t.Name = "Main";
+            Console.WriteLine(t.Name);
+            Console.WriteLine(t.IsAlive);
+            Console.WriteLine(t.Priority);
+            Console.WriteLine(t.ThreadState);
+            Console.WriteLine(Thread.GetDomain().FriendlyName); // Имя домена, т.е. файла в котором запущен тред
+            Console.ReadKey();*/
+
+
+
+            /* Thread t1 = new Thread(func);
+             t1.Priority = ThreadPriority.Lowest;
+             t1.Start();
+
+             for (int i = 0; i < 10; i++)
+             {
+                 Console.WriteLine("Поток 1 выводит " + i);
+                 Thread.Sleep(0);
+             }
+             Console.ReadKey();
+
+         }
+
+         static void func()
+         {
+             for (int i = 0; i < 10; i++)
+             {
+                 Console.WriteLine("Поток 2 выводит " + i);
+                 Thread.Sleep(0);
+             }
+         }*/
+
+            /*Thread t1 = new Thread(func);
+            t1.Name = "T1";
+            t1.Start();
+            Thread t2 = new Thread(func);
+            t2.Name = "T2";
+            t2.Start();*/
+
+            Thread[] threads = new Thread[3];
+            for(int i =0; i<3; i++)
+            {
+                threads[i] = new Thread(func);
+                threads[i].Name = "Thread" + i;
+                threads[i].Start();
+            }
+            Console.ReadKey();
+        }
+
+        static void func()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+                Thread.Sleep(0);
+            }
+        }
+    }
+}
---
+++ Week7/Task2/Task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Task2
+{
+    public class myThread
+    {
+        Thread thr;
+        public myThread(string name)
+        {
+            thr = new Thread(threadField);
+            thr.Name = name;
+        }
+
+        public void Start()
+        {
+            thr.Start();
+        }
+
+        public void threadField()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + ": "+ (i+1));
+                Thread.Sleep(0);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            myThread t1 = new myThread("Thread1");
+            myThread t2 = new myThread("Thread2");
+            myThread t3 = new myThread("Thread3");
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
